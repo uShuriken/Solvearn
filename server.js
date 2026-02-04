3eeefa9467c42abea7f0d04a8d90e1fbf71f2946
Liam <shurikenninja01business@gmail.com>
2026-02-05 00:30:14 +1300

    aesthetic spacing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 // Serve static files from the current directory
 app.use(express.static(__dirname));
